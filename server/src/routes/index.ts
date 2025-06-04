d5dac5fd717c3f1443179b27ac1ab78927cec384
Sonu Toor <sonu@sonus-mbp-3.home>
2025-06-04 17:50:31 -0400

    rejig one mo time

---
+++ server/src/routes/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import userRoutes from './user/userRoutes';
+import contactRoutes from './contact/contactRoutes';
+
+const router = express.Router();
+
+// Mount sub-routes
+router.use('/user', userRoutes);
+router.use('/contact', contactRoutes);
+
+export default router; 
