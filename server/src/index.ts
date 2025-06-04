64572140270ffb1f175f1561ffd4cb7d1266f5c8
Sonu Toor <sonu@sonus-mbp-3.home>
2025-06-04 17:46:51 -0400

    rejig routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user/userRoutes';
-import contactRoutes from './routes/contact/contactRoutes';
+import apiRoutes from './routes/apiRoutes';
 
 dotenv.config();
 const app = express();
@@ -11,9 +10,8 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/user', userRoutes);
-app.use('/contact', contactRoutes);
+// Mount API routes
+app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
     res.send('Express server is running');
